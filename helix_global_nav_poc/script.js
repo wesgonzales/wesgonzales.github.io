695794ed8671f8ba492c4af9d55563c277c51550
wesgonzales <ws.brndn@yahoo.com>
2015-07-22 09:42:00 -0400

    Helix POC
    Sticky POC

---
+++ helix_global_nav_poc/script.js
@@ -0,0 +1,39 @@
+(function init()
+{
+    $('.menu').addClass('original')
+        .clone()
+        .insertAfter('.menu')
+        .addClass('cloned')
+        .css('position', 'fixed')
+        .css('top', '0')
+        .css('margin-top', '0')
+        .css('z-index', '500')
+        .removeClass('original')
+        .hide();
+
+    scrollIntervalID = setInterval(stickIt, 10);
+})();
+
+function stickIt() {
+
+    var orgElementPos = $('.original').offset(),
+        orgElementTop = orgElementPos.top;
+
+    if ($(window).scrollTop() >= (orgElementTop))
+    {
+        var orgElement = $('.original'),
+            coordsOrgElement = orgElement.offset(),
+            leftOrgElement = coordsOrgElement.left,
+            widthOrgElement = orgElement.css('width');
+
+        $('.cloned').css('left', leftOrgElement + 'px')
+                    .css('top', 0)
+                    .css('width', widthOrgElement)
+                    .show();
+
+        $('.original').css('visibility', 'hidden');
+    } else {
+        $('.cloned').hide();
+        $('.original').css('visibility', 'visible');
+    }
+};
